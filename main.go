205c2bc76376153e77744e79b9cdf7f0ca6f3324
supremex04 <bhandarisupreme7@gmail.com>
2024-10-22 17:20:45 +0545

    pratt parser

--- main.go
+++ main.go
@@ -8,13 +8,12 @@ import (
 	"run/repl"
 )
 
-func main(){
+func main() {
 	user, err := user.Current()
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("Hey %s! This is Run programming language!\n", user.Username)
-	fmt.Printf("Type commands here:\n")
-	repl.Start(os.Stdin/*, os.Stdout*/)
+	repl.Start(os.Stdin /*, os.Stdout*/)
 }
--- repl/repl.go
+++ repl/repl.go
@@ -7,19 +7,35 @@ import (
 	"run/lexer"
 	"run/token"
 )
-const PROMPT = ">> "
 
+const RUN_BANNER = `
+      ___           ___           ___     
+     /  /\         /__/\         /__/\    
+    /  /::\        \  \:\        \  \:\   
+   /  /:/\:\        \  \:\        \  \:\  
+  /  /:/~/:/    ___  \  \:\   _____\__\:\ 
+ /__/:/ /:/___ /__/\  \__\:\ /__/::::::::\
+ \  \:\/:::::/ \  \:\ /  /:/ \  \:\~~\~~\/
+  \  \::/~~~~   \  \:\  /:/   \  \:\  ~~~ 
+   \  \:\        \  \:\/:/     \  \:\     
+    \  \:\        \  \::/       \  \:\    
+     \__\/         \__\/         \__\/    
+`
+const PROMPT = ">> "
 
 func Start(in io.Reader /*, out io.Writer*/) {
 	scanner := bufio.NewScanner(in)
 
 	for {
+		fmt.Printf(RUN_BANNER)
+		fmt.Printf("\nType commands here:\n")
 		fmt.Printf(PROMPT)
-		// The scanner.Scan() method reads the next line from the input, 
-		//breaking the input into tokens based on newlines 
+
+		// The scanner.Scan() method reads the next line from the input,
+		//breaking the input into tokens based on newlines
 		// (by default, it uses a newline \n as the delimiter).
 		scanned := scanner.Scan()
-		// The scanner.Scan() call returns true if there is another line to read, 
+		// The scanner.Scan() call returns true if there is another line to read,
 		//or false if the input has ended (like if EOF is encountered).
 		if !scanned {
 			return
@@ -32,4 +48,3 @@ func Start(in io.Reader /*, out io.Writer*/) {
 		}
 	}
 }
-			
