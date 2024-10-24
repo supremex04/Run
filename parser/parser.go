0cb9b002293c453bd5c1ddb048a533170e1925f2
supremex04 <bhandarisupreme7@gmail.com>
2024-10-24 19:52:39 +0545

    parsing boolean expression & testing

--- parser/parser.go
+++ parser/parser.go
@@ -89,6 +89,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.NOT_EQ, p.parseInfixExpression)
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
+	p.registerPrefix(token.TRUE, p.parseBoolean)
+	p.registerPrefix(token.FALSE, p.parseBoolean)
 
 	p.nextToken()
 	p.nextToken()
@@ -254,4 +256,8 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 	p.nextToken()
 	expression.Right = p.parseExpression(precedence)
 	return expression
+}
+
+func (p *Parser) parseBoolean() ast.Expression {
+	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
 }
