05f5b4018323d3358ea56bbeb8af418c12363676
supremex04 <bhandarisupreme7@gmail.com>
2024-11-02 21:56:37 +0545

    object

--- object/object.go
+++ object/object.go
@@ -1,7 +1,17 @@
 package object
 
+import (
+	"fmt"
+)
+
 type ObjectType string
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
